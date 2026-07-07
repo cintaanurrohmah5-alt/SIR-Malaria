0f381705417d7a3dce2c01cbaed02c9d4974cef6
cintaanurrohmah5-alt <cintaanurrohmah5@gmail.com>
2026-07-07 15:04:17 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ DATABASE_WILAYAH = {
     }
 }
 
-st.title("📊 Sistem Simulasi Malaria (Model SIR)")
+st.title("📊 KARISMA : Kalkulator Analisis Risiko Infeksi Simulasi Malaria")
 st.markdown("Aplikasi interaktif pemodelan dinamika malaria berbasis iklim untuk Provinsi Papua Tengah & Wilayah Universal.")
 
 with st.expander("Pelajari Model Matematika SIR yang Digunakan"):
